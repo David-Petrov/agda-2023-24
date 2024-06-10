46efa0ac8c3bfeb8b99f758fde524ea5a6080fcb
David Petrov <david.petrov@limechain.tech>
2024-06-10 14:01:15 +0300

    feat: add nix flake and .envrc

---
+++ devshell/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    } @ inputs : flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+      in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            ### Main
+
+            (pkgs.agda.withPackages (ps: [
+              ps.standard-library
+            ]))
+            pkgs.ghc
+            pkgs.stack
+          ];
+        };
+    });
+}
